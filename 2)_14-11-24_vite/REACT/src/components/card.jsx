61c524ad023da995448224d2ac1d6d04d22f0443
[Siddhant Jain] <sidjain1387@gmail.com>
2024-11-15 08:29:43 +0530

    react using vite and componenets

---
+++ 2)_14-11-24_vite/REACT/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import reactLogo from '../assets/react.svg'
+
+
+const Card = () => {
+    return (
+        <div>
+            <h2>Siddhant Jain</h2>
+            <h2>Roll No. : 2200320230053</h2>
+            <img src={reactLogo} alt="" />
+        </div>
+    )
+}
+
+export default Card
