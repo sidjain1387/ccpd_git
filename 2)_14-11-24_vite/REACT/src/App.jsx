d1d5912a881dd355e1e470ff5b0c1552818580b1
[Siddhant Jain] <sidjain1387@gmail.com>
2024-11-18 12:16:59 +0530

    props done in react

--- 2)_14-11-24_vite/REACT/src/App.jsx
+++ 2)_14-11-24_vite/REACT/src/App.jsx
@@ -3,13 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/card'
+import pic1 from "./assets/download.jpg"
+import pic2 from "./assets/download1.jpg"
+import pic3 from "./assets/download2.jpg"
+import pic4 from "./assets/download3.jpg"
+
 
 
 function App() {
   return (
-    <>
-      <Card />
-    </>
+    <div className='aa'>
+      <Card name="siddhant" roll="4434343" img={pic1}/>
+      <Card name="akhil" roll="12212112" img={pic2}/>
+      <Card name="nitin" roll="233322332" img={pic3}/>
+      <Card name="gagan" roll="5455454" img={pic4}/>
+    </div>
   )
 }
 
--- 2)_14-11-24_vite/REACT/src/components/card.jsx
+++ 2)_14-11-24_vite/REACT/src/components/card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import reactLogo from '../assets/react.svg'
+import "./card.css"
 
-
-const Card = () => {
+const Card = (props) => {
     return (
-        <div>
-            <h2>Siddhant Jain</h2>
-            <h2>Roll No. : 2200320230053</h2>
-            <img src={reactLogo} alt="" />
+        <div className='card'>
+            <h2>{props.name}</h2>
+            <h2>Roll No. : {props.roll}</h2>
+            <img src={props.img} alt="" width="300px" height="200px"/>
         </div>
     )
 }
